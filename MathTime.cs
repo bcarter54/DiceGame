b37cf56e6fd6c10b1ca1067d13f78e58ab1d1674
bcarter54 <carterbryce54@gmail.com>
2025-01-16 21:48:21 -0700

    Made RollDice method and used it in Run method

--- MathTime.cs
+++ MathTime.cs
@@ -1,23 +1,38 @@
 namespace DiceGame;
 
+//MathTime class is in charge of handling the dice rolls as well as providing the array with the sums of the rolls
 public class MathTime
 {
+    //Run method for the actual rolls and tallying
     public static int[] Run(int numRolls)
     {
+        //Initialize array
         int[] rollTallies = new int[11];
 
+        //For loop designed to run for as many rolls as requested by the user
         for (int iCount = 0; iCount < numRolls; iCount++)
         {
+            // Randomization represented by the variable dice used to simulate dice rolls
             Random dice = new Random();
-            int dice1 = dice.Next(1, 7);
-            int dice2 = dice.Next(1, 7);
+            int dice1 = RollDice();
+            int dice2 = RollDice();
+            
+            // Add up dice rolls
             int sum = dice1 + dice2;
 
-
+            // Place tally in array
             rollTallies[sum - 2]++;
         }
-        
+        // Return completed array
         return(rollTallies);
         
+    }
+
+    public static int RollDice()
+    {
+        Random rnd = new Random();
+        int dice = rnd.Next(1, 7);
+        
+        return dice;
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,22 +7,27 @@ internal class Program
     public static void Main(string[] args)
     {
         
-
+        // Introduction and asking for number of rolls
         Console.WriteLine("Welcome to the dice throwing simulator!\n\n" +
                           "How many dice rolls would you like to simulate?");
 
         int numRolls = int.Parse(Console.ReadLine());
 
+        // Call MathTime class and run the Run method to give the array rollTallies its values
         int[] rollTallies = MathTime.Run(numRolls);
         
+        // With the values, print out how many times each number was rolled and make a histogram using the results
         for (int tally = 0; tally < rollTallies.Length; tally++)
         {
-            Console.WriteLine($"Sum of {tally + 2} was rolled {rollTallies[tally]} times");
+            Console.WriteLine($"{tally + 2} was rolled {rollTallies[tally]} times");
+            
+            // Calculate what percentage of the rolls that number made up and use that percentage to determine the number of stars
             double percent = ((double)rollTallies[tally] / numRolls) * 100;
-
             int numStars = (int)Math.Round(percent);
-
+            // Add an * equal to the amount of percentage that number makes up the rolls
             string stars = new string('*', numStars);
+            
+            // Print the stars along with the percentage 
             Console.WriteLine($"{tally + 2}: {stars} ({percent}%)");
         }
     }
